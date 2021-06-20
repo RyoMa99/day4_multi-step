84b545586e00efbef197a9185a859461e0c7ebba
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-06-20 05:59:42 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div
+    id="app"
+    class="container mt-5"
+  >
+    <router-view />
   </div>
 </template>
 
